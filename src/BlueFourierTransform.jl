5703bd054742f33dd58258d236285a2f429fd5c6
G Jake Gebbie <ggebbie@whoi.edu>
2026-09-16 12:58:39 -0400

    fits synthetic data

--- src/BlueFourierTransform.jl
+++ src/BlueFourierTransform.jl
@@ -2,7 +2,10 @@ module BlueFourierTransform
 
 using SpectraFromScratch
 using BLUEs
-    
+using OffsetArrays
+
+import SpectraFromScratch: FourierTransform
+
 # Write your package code here.
 # function blue_fourier_transform(
 #     func, # takes input and get obs
@@ -22,7 +25,8 @@ function blue_fourier_transform(
     # issue: what are the units?
     # maybe passing the first guess will solve it
     E = impulse_response(x0.v, func)
-
+    println(E)
+    
     # necessary?
     # first-guess prediction of the obs
     # y0 = E*x0
@@ -34,11 +38,14 @@ end
 
 function construct_first_guess(Ψ, σ2mean)
     ffundamental = first(Ψ.freq) # fundamental frequency
-    N = 2length(Ψ.psi) # assume even number of points
+    n = length(Ψ.psi)
+    N = 2n # assume even number of points
     T = 1/ffundamental # repeat interval
     
     # A = amplitude of waves (?)
-    A0 = sqrt.(Ψ.psi .* (N^2 / 2T))
+    # some care is taken for singular Nyquist frequency
+    A0 = vcat(sqrt.(Ψ.psi[1:end-1] .* (N^2 / 2T)),
+              sqrt.(Ψ.psi[end] .* (N^2 / T)))  
 
     sample_state_val = sqrt(first(Ψ.psi)*first(Ψ.freq))
     vx0 = zeros(eltype(sample_state_val),N)
@@ -51,18 +58,49 @@ function construct_first_guess(Ψ, σ2mean)
 end
 
 function impulse_response(input0, func)
-	output0 = vec(func(input0))
-	E = zeros(eltype(first(output0)/first(input0)), length(output0), length(input0))
+    output0 = vec(func(input0))
+    E = zeros(eltype(first(output0)/first(input0)), length(output0), length(input0))
+    input = deepcopy(input0)
+    for i in 1:length(input0)
+	delta = one(eltype(input))
 
-	input = deepcopy(input0)
-	for i in 1:length(input0)
-		delta = 1.0*unit(first(input))
-		input[i] += delta
-		E[:,i] = (vec(func(input)) -
-			vec(output0))./delta
-		input[i] -= delta
-	end
-	return E
+	# delta = 1.0*unit(first(input))
+	input[i] += delta
+	E[:,i] = (vec(func(input)) -
+		  vec(output0))./delta
+	input[i] -= delta
+    end
+    return E
+end
+
+function SpectraFromScratch.FourierTransform(v::Vector, df::Number)
+    N = length(v)
+    n = SpectraFromScratch.fourier_modes(N) 
+
+    # if iseven(N)
+    #     n = -(N/2):((N/2)-1)
+    # else
+    #     n = -(N/2):((N/2))
+    # end
+
+    sample = first(v) + im*first(v)
+    coeff = OffsetArray( fill( zero(eltype(sample)), N), n)
+
+    N2 = Integer(N/2)
+    
+    # zero frequency coefficient is real
+    coeff[0] = v[1]
+
+    # Nyquist frequency coefficient is real
+    coeff[-N2] = v[N2+1]
+
+    # positive frequencies are complex
+    coeff[1:N2-1] = v[2:N2] + im * v[(N2 + 2): end]
+
+    # negative frequencies are a complex conjugate
+    coeff[-1:-1:-N2+1] = v[2:N2] - im * v[(N2 +2):end]
+
+    return FourierTransform(coeff, df)
 end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,80 @@
+using Revise
 using BlueFourierTransform
 using Test
+using BLUEs
+using SpectraFromScratch
+using OffsetArrays
 
 @testset "BlueFourierTransform.jl" begin
-    # Write your tests here.
+
+    ## make a power law (sample frequency spectrum)
+
+    # record length/ simulation length.
+    T = 1000 # repeat interval
+    N = 10 # number of points on regularly sampled grid
+    β = 2.0  # power law 
+    Δt = T/N # time interval of regular sampling
+    Δf = Δt/T # Δf = bandwidth
+    ffundamental = 1/T # fundamental frequency
+    fnyquist = 1/2Δt # Nyquist frequency
+    σ2 = 1.0  # estimate of total variance
+    n = SpectraFromScratch.fourier_modes(N)
+    nf = maximum(abs.(n))
+    f_positive = (1:nf)/T #  goes between fundamental and Nyquist frequencies
+    Ψ = spectral_power_law(f_positive, β, σ2) # power-law frequency spectrum
+
+    ## make regular timeseries
+    x̂true = SpectraFromScratch.FourierTransform(Ψ)
+    xtrue = SpectraFromScratch.RegularTimeseries(x̂true)
+    @test isapprox(sum(xtrue.x), 0.0, atol=1e-10)
+        
+    ## will need to independently decide on the mean value
+    # (i.e., no info in frequency spectrum)
+    # here the mean is zero
+    
+    ## make irregular samples (using linear interpolation)
+    T =  maximum(xtrue.time) - minimum(xtrue.time)
+    M = 5; # number of observations
+    t = rand(M)*T
+    
+    ## save samples as "perfect observations"
+    # save the function that makes samples (linear interpolation)
+    irregular_sample_fourier_transform(t) = [expand(t[i],x̂true) for i in eachindex(t)]
+    ytrue = irregular_sample_fourier_transform(t)
+
+    # also need the function for control variables (later move into master function)
+    irregular_sample_control_variables(x) = [expand(t[i],FourierTransform(x,Δf)) for i in eachindex(t)]
+    # y0 = irregular_sample_control_variables(u0.v) # interesting test but wrong scope to keep it
+    
+    # contaminate observations with noise
+    σn = 0.01
+    n = σn * randn(M)
+    y = ytrue .+ n
+    
+    # save samples and expected noise together in an `Estimate`
+    y_estimate = Estimate(y, fill(σn, M))
+
+    # solve for BLUE of Fourier Transform
+    u = BlueFourierTransform.blue_fourier_transform(
+        irregular_sample_control_variables, # takes input and get obs
+        y_estimate, # observations
+        Ψ, # first guess frequency spectrum
+        1.0) # uncertainty of mean value, not given by spectrum
+
+    # how well does it recontruct the obs?
+    ỹ = irregular_sample_control_variables(u.v)
+    @test sqrt(sum(((y-ỹ)/σn).^2)/M) < 1
+
+    # how close is the Fourier Transform description of the data?
+    # they actually look pretty different
+    x̂̃ = FourierTransform(u.v, Δf)
+    Δx_real = real.(x̂true.coeff) .- real.(x̂̃.coeff)
+    Δx_imag = imag.(x̂true.coeff) .- imag.(x̂̃.coeff)
+
+    # is it within the error bars? Would have to propagate the errors through `FourierTransform`
+    
+    # how faithful is it to the input spectrum?
+    Ψ̃ = periodogram(x̂̃)    
+
+    
 end
